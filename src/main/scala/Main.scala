3f3a3f9f85264ae3575bb09820681746066d86e9
colewarner24 <74276573+colewarner24@users.noreply.github.com>
2022-03-14 17:26:30 -0700

    made the log-times file to graph accesses over time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,11 +10,16 @@ import org.apache.spark.sql.execution.streaming.state
 import org.json4s.scalap.scalasig.ClassFileParser.state
 import spire.math.Polynomial.x
 
+import java.io.{File, PrintWriter}
 import scala.collection._
 
 
 object Main {
   def main(args: Array[String]): Unit = {
+
+//    System.setProperty("hadoop.home.dir", "c:/winutils/")
+
+//    System.setProperty("hadoop.home.dir", "C:/hadoop")
 
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -24,19 +29,21 @@ object Main {
 
     val sc = new SparkContext(conf)
 
+//    val log = sc.textFile("access.log")
     val log = sc.textFile("access.log")
-//    val log = sc.textFile("test")
 
-    val logLines = log.map(line => (line.split(" ")(3).trim(), line.split(" ")(0).trim))
+    val logLines = log.map(line => (line.split(" ")(3).trim().substring(1), line.split(" ")(0).trim))
 
     val result = logLines.groupByKey().map({case (time, ips) => (time, ips.size)}).sortBy(_._1)
 
 
-    result.foreach(println)
+//    result.saveAsTextFile("log-times");
 
+    val pw = new PrintWriter(new File("log-times.csv"))
+    val list = result.collect()
+    list.foreach({case (time, num) => pw.write(time + ", " + num.toString + "\n")})
 
-
-
+    pw.close()
   }
 }
 
